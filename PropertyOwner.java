b3597b0675177fa490138caae2a7e978d6cedac1
tuyetdng <dangthianhtuyettpk2019@gmail.com>
2023-11-11 16:17:26 +0700

    Tuyet_Property&PropertyOwner

---
+++ PropertyOwner.java
@@ -0,0 +1,64 @@
+package main;
+
+import java.util.ArrayList;
+
+public class PropertyOwner extends User {
+
+    // Data members
+    private double totalPropertyValue;
+    private Property property = new Property();
+    private ArrayList<Property> ownedProperties = property.restoreDataFromChar();
+
+    // Constructors
+    public PropertyOwner() {
+
+    }
+
+    public PropertyOwner(String userID, String firstName, String lastName, String email, String password) {
+        super(userID, firstName, lastName, email, password);
+    }
+
+    public double getTotalPropertyValue() {
+        return totalPropertyValue;
+    }
+
+    public void setTotalPropertyValue(double totalPropertyValue) {
+        this.totalPropertyValue = totalPropertyValue;
+    }
+
+    public ArrayList<Property> getOwnedProperties() {
+        return ownedProperties;
+    }
+
+    public void setOwnedProperties(ArrayList<Property> ownedProperties) {
+        this.ownedProperties = ownedProperties;
+    }
+
+    // Methods
+    public ArrayList<Property> ListProperty() {
+        return ownedProperties;
+    }
+
+    public boolean CreateProperty(String propertyID, String description, String rentalRate) {
+        Property newProperty = new Property(propertyID, description, rentalRate);
+        if (property.createProperty(newProperty)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean UpdateProperty(Property updateProperty) {
+        if (property.updateProperty(updateProperty)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void DeleteProperty(String id) {
+        property.deleteProperty(id);
+
+    }
+
+}
---
+++ User.java
@@ -0,0 +1,84 @@
+package main;
+
+public class User {
+
+    //Data members
+    protected String userID;
+    protected String firstName;
+    protected String lastName;
+    protected String email;
+    protected String password;
+
+    //constructors
+    public User() {
+
+    }
+
+    public User(String userID, String firstName, String lastName, String email, String password) {
+        this.userID = userID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+
+    }
+
+    //Methods
+    public void register() {
+
+    }
+
+    public void Login() {
+
+    }
+
+    public void updateProfile() {
+
+    }
+
+    public void resetPassword() {
+
+    }
+
+//get set method
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
